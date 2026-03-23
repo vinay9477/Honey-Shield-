f44b989c58cad63a0be563befe19632a1b8a6997
Vinay Goud <vinaygoud0931@gmail.com>
2026-03-23 20:11:44 +0530

    Update frontend config

--- frontend/public/js/config.js
+++ frontend/public/js/config.js
@@ -1,7 +1,8 @@
 // Dynamic API configuration - works on localhost, LAN, and production
 function isLocal() {
   const h = window.location.hostname;
-  return h === 'localhost'
+  return window.location.protocol === 'file:'
+    || h === 'localhost'
     || h === '127.0.0.1'
     || h === '' // For file:/// protocol
     || h.startsWith('192.168.')
@@ -9,10 +10,11 @@ function isLocal() {
     || /^172\.(1[6-9]|2\d|3[01])\./.test(h);
 }
 
+const host = window.location.hostname || '127.0.0.1';
 const API_BASE = isLocal()
-  ? 'http://' + window.location.hostname + ':3000'
+  ? 'http://' + host + ':3000'
   : 'https://honey-shield-production.up.railway.app';
 
 const HONEYPOT_REDIRECT = isLocal()
-  ? 'http://' + window.location.hostname + ':4000'
+  ? 'http://' + host + ':4000'
   : '/';
