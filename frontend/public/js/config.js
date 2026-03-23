0099f2e217c8399a0bac7621549dd7970cc83dda
Vinay Goud <vinaygoud0931@gmail.com>
2026-03-23 20:00:11 +0530

    fix: allow local html file execution by updating config.js API base

--- frontend/public/js/config.js
+++ frontend/public/js/config.js
@@ -3,6 +3,7 @@ function isLocal() {
   const h = window.location.hostname;
   return h === 'localhost'
     || h === '127.0.0.1'
+    || h === '' // For file:/// protocol
     || h.startsWith('192.168.')
     || h.startsWith('10.')
     || /^172\.(1[6-9]|2\d|3[01])\./.test(h);
